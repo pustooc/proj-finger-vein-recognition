bf502426ced60bc90fdb955b46a1474e27b8a63d
infoomics <sbx_94@hotmail.com>
2025-01-01 19:15:38 +0000

    Initial structure

---
+++ src/index.py
@@ -0,0 +1,21 @@
+def prepare_data():
+    pass
+
+
+def build_custom_cnn():
+    pass
+
+
+def fit_model():
+    pass
+
+
+def evaluate_model():
+    pass
+
+
+if __name__ == '__main__':
+    prepare_data()
+    build_custom_cnn()
+    fit_model()
+    evaluate_model()
