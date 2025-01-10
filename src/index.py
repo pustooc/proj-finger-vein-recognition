14ef44b564c8a27b7e5f0afdc91c99ac26164de3
infoomics <sbx_94@hotmail.com>
2025-01-10 19:37:51 +0000

    Implement dataframes for the images to flow_with_dataframe

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import pandas as pd
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 
@@ -8,7 +9,9 @@ from tensorflow.keras.preprocessing.image import img_to_array
 def train_validate_test_split():
     '''
     Determine which image files go into the train-validate-test sets.
-    Hyperparameters: ratio of the split.
+    Returns: three dataframes representing the train-validate-test split, each with an
+    image_file column (x) and a person_id column (y).
+    Hyperparameters: ratio of the split; number of images per person.
     '''
 
     # Group images by person ID
@@ -23,28 +26,45 @@ def train_validate_test_split():
     # For each person, split her images into train, validation, and test sets
     # according to a ratio
     train_images = []
+    train_labels = []
     validate_images = []
+    validate_labels = []
     test_images = []
+    test_labels = []
     for person_id, images in images_by_person.items():
+        # Randomise the order of image selection with a seed for reproducibility
         np.random.seed(person_id)
         np.random.shuffle(images)
-        # train:validate:test ratio is 60:20:20
+        # 60:20:20 is the train:validate:test ratio
+        # 72 images per person
         TRAIN_COUNT = 43
         VALIDATE_COUNT = 14
-        # TEST_COUNT = 15
+        TEST_COUNT = 15
         train_images.extend(images[:TRAIN_COUNT])
+        train_labels.extend([person_id] * TRAIN_COUNT)
         validate_images.extend(images[TRAIN_COUNT: TRAIN_COUNT + VALIDATE_COUNT])
+        validate_labels.extend([person_id] * VALIDATE_COUNT)
         test_images.extend(images[TRAIN_COUNT + VALIDATE_COUNT:])
+        test_labels.extend([person_id] * TEST_COUNT)
+    train_df = pd.DataFrame({
+        'image_file': train_images,
+        'person_id': train_labels
+    })
+    validate_df = pd.DataFrame({
+        'image_file': validate_images,
+        'person_id': validate_labels
+    })
+    test_df = pd.DataFrame({
+        'image_file': test_images,
+        'person_id': test_labels
+    })
 
     # Log the results of the split
-    with open('data/logs/train_images.txt', 'w') as f:
-        f.write('\n'.join(train_images))
-    with open('data/logs/validate_images.txt', 'w') as f:
-        f.write('\n'.join(validate_images))
-    with open('data/logs/test_images.txt', 'w') as f:
-        f.write('\n'.join(test_images))
+    train_df.to_csv('data/logs/train_df.csv', index=False)
+    validate_df.to_csv('data/logs/validate_df.csv', index=False)
+    test_df.to_csv('data/logs/test_df.csv', index=False)
 
-    return train_images, validate_images, test_images
+    return train_df, validate_df, test_df
 
 
 def load_images(file_list):
@@ -79,7 +99,7 @@ def evaluate_model():
 
 
 if __name__ == '__main__':
-    train_images, validate_images, test_images = train_validate_test_split()
+    train_df, validate_df, test_df = train_validate_test_split()
     load_images()
     define_custom_cnn()
     train_model()
