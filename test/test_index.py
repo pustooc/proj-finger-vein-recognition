41c00e5f1a516b905964aa3fa59d6e396b5838e0
infoomics <sbx_94@hotmail.com>
2025-01-11 03:01:03 +0000

    Add test for one hot encoding and extra comments

--- test/test_index.py
+++ test/test_index.py
@@ -10,13 +10,24 @@ def test_train_validate_test_split_returns_correct_dataframes():
     validate_columns = list(validate_df.columns)
     test_columns = list(test_df.columns)
 
+    # Check column headers
     assert train_columns[0] == 'image_file'
     assert train_columns[1] == 'person_id'
     assert validate_columns[0] == 'image_file'
     assert validate_columns[1] == 'person_id'
     assert test_columns[0] == 'image_file'
     assert test_columns[1] == 'person_id'
-    # Number of rows
+    # Check number of rows
     assert train_df.shape[0] == 4300
     assert validate_df.shape[0] == 1400
     assert test_df.shape[0] == 1500
+
+
+def test_load_images_returns_correct_generators():
+    train_df, validate_df, test_df = train_validate_test_split()
+    train_generator, validate_generator, test_generator = load_images(train_df, validate_df, test_df)
+
+    # Check that one hot encoding worked
+    assert train_generator.num_classes == 100
+    assert validate_generator.num_classes == 100
+    assert test_generator.num_classes == 100
